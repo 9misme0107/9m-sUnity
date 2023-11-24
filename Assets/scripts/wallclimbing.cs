da06bf7062deac0b03bdc5599f151803a3f3fa1a
9misme0107 <chenjohan9217@gmail.com>
2023-11-24 17:55:38 +0800

    123

---
+++ Assets/scripts/wallclimbing.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wallclimbing : MonoBehaviour
+{
+    public LayerMask whatiswall;
+    public LayerMask whatisground;
+    [Header("input")]
+    private float horizontalinput;
+    private float verticalinput;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
